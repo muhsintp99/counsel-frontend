1ff75b1d3f977d46d2ac469d9b58b35994b6bcb1
muhsin <muhsintp.develop@gmail.com>
2025-06-11 15:26:07 +0530

    saslprep--11

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,9 +9,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
-      // this sets a default port to 3000
       port: PORT,
       host: true,
       proxy: {
