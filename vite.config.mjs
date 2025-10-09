1f83dcd7fdea10e9d384b2583edc7f36b85c6060
muhsin <muhsintp.develop@gmail.com>
2025-10-09 15:24:02 +0530

    hello world

--- vite.config.mjs
+++ vite.config.mjs
@@ -16,14 +16,14 @@ export default defineConfig(({ mode }) => {
       host: true,
       proxy: {
         '/public': {
-          target: 'http://localhost:5050',
+          target: 'https://api.edspiriainternational.com',
           // target: 'https://counsel-backend-z72e.onrender.com',
           changeOrigin: true,
           rewrite: (path) => path,
         },
         '/countries': {
           // target: 'https://counsel-backend-z72e.onrender.com',
-          target: 'http://localhost:5050',
+          target: 'https://api.edspiriainternational.com',
           changeOrigin: true,
           rewrite: (path) => path,
         },
